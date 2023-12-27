0a59a3200a6d67077551d269a55cc0c7c8f89a3c
marterik <73703880+marterik@users.noreply.github.com>
2023-12-27 22:30:26 +0100

    colors

--- sumtest.py
+++ sumtest.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python3
 
-"""Programm to compare sha256 sums"""
+"""
+Programm to compare sha256 sums.
+This Program takes 2 inputs: First file to hash and second the checksum from webpage.
+Script must be used in the folder where the file is placed.
+"""
 
 import sys
 import hashlib
 import os
 
+
+class Colors:
+    """Color Class for ASCII Terminal"""
+    green = "\033[0;32m"
+    red = "\033[0;31m"
+    end = "\033[00m"
+
+
 to_sum = sys.argv[1]
 
 comp_sum = sys.argv[2]
 
-to_sum_file = os.path.join(os.curdir, to_sum)
 
-print("\nDie Datei die getestet wird: ")
-print(to_sum_file + "\n")
+def main(to_sum, comp_sum):
+    to_sum_file = os.path.join(os.curdir, to_sum)
 
-with open(to_sum_file, "rb") as f:
-    is_sum = hashlib.sha256(f.read())
+    print("\nDie Datei die getestet wird: ")
+    print(to_sum_file + "\n")
 
-print("Der erstellte sha256 Haswert: ")
-print(is_sum.hexdigest() + "\n")
+    with open(to_sum_file, "rb") as f:
+        is_sum = hashlib.sha256(f.read())
+
+    print("Der erstellte sha256 Haswert: ")
+    print(is_sum.hexdigest() + "\n")
+
+    if is_sum.hexdigest() == comp_sum:
+        print(f"\n{Colors.green}Hashwerte sind identisch!!!{Colors.end}")
+    else:
+        print(f"\n{Colors.red}Hashes stimmen nicht überein!!!{Colors.end}")
 
 
-if is_sum.hexdigest() == comp_sum:
-    print("\nHashwerte sind identisch")
-else:
-    print("\nHashes stimmen nicht überein!!!")
+main(to_sum, comp_sum)
